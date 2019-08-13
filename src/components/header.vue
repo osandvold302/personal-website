4ef9d19b40d5f531190e1f3803f0e41f72dc1707
Olivia Sandvold <ollie302@gmail.com>
2019-08-13 09:57:53 -0500

    Remove changing path on links

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,9 +4,9 @@
         <b-navbar-toggle class="ml-auto navbar-light" target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="navbar-nav ml-auto text-center">
-            <b-nav-item href="/about">About</b-nav-item>
-            <b-nav-item href="/projects">Projects</b-nav-item>
-            <b-nav-item href="/resume">Resume</b-nav-item>
+            <b-nav-item href="/">About</b-nav-item>
+            <b-nav-item href="/">Projects</b-nav-item>
+            <b-nav-item href="/">Resume</b-nav-item>
           </b-navbar-nav>   
         </b-collapse>
         </b-navbar>
