3ed64179494d3438c8efebfdcac814e06c17593f
Olivia Sandvold <ollie302@gmail.com>
2019-08-13 15:33:55 -0500

    Add projects section

---
+++ src/components/projects.vue
@@ -0,0 +1,22 @@
+<template>
+    <div b-container class="container-projects">
+        <h2>Projects</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'projects',
+    components: {}
+}
+</script>
+
+<style scoped>
+.container-projects {
+    padding-top: 20px;
+}
+
+h2 {
+    padding-left: 100px;
+}
+</style>
