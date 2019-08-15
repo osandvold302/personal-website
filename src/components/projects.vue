569d13a87577219d042175e527deab703da4269e
Olivia Sandvold <ollie302@gmail.com>
2019-08-15 09:41:41 -0500

    New project and github link overlay

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,5 +1,9 @@
 <template>
     <div b-container class="container-projects">
+       <link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
+        crossorigin="anonymous"> 
         <b-row>
             <b-col class="col-text text-left" offset="1">
                 <h2>Projects</h2>
@@ -7,8 +11,14 @@
         </b-row>
         <template v-for="project in projects" id="project-section">
             <b-form-row class="project-rows" v-bind:key="project.title"> 
-            <b-col class="justify-content-md-center text-center" offset-sm="1" sm="2">
-                <img v-bind:src="getImgUrl(project.img)" class="img-fluid" v-bind:alt="project.img" width=auto/>
+            <b-col id="project-image" class="justify-content-md-center text-center" offset-sm="1" sm="2">
+                <img v-if="project.img" v-bind:src="getImgUrl(project.img)" class="img-fluid" v-bind:alt="project.img" width=auto/>
+                <img v-else v-bind:src="getImgUrl('logo.png')" class="img-fluid" width=auto/>
+                <div class="overlay">
+                    <a v-if="project.github_link" :href="project.github_link" class="icon" title="GitHub Link">
+                        <i class="fab fa-github fa-4x"></i>
+                    </a>
+                </div>
             </b-col>
             <b-col id="project-summary" class="col-text text-left" sm="8">
                 <h4>{{ project.title }}</h4>
@@ -22,30 +32,48 @@
 
 <script>
 export default {
-    name: 'projects',
-    components: {},
-    methods: {
-        getImgUrl(pic) {
-            return require('../assets/'+pic)
-        }
-    },
-    data() {
-        return {
-            projects: [
-                {title: 'iOS Test Lab App', summary: 'The Tamino iOS Photo application was built to detect deformities and heat ' 
-                + 'variations of our high temperature furnaces. I built the backend using Swift and AWS tools to connect the user ' 
-                + 'to the DynamoDB table and upload images to an S3 bucket. From there, a lambda function was triggered and detected ' 
-                + 'parts using a pre-built neural network.', img: 'takephotoapp.png'},
-                {title: 'My Little Trader Bot', summary: 'My Little Trader Bot began from a desire to learn and understand financial '
-                + 'trading algorithms based on the challenged provided by Optiver at HackDelft 2019 in Delft, Netherlands. '
-                + 'My team researched various types of strategies and played the "fake" stocks, attempting to increase our profits. '
-                + 'We were able to build a front-end website for monitoring our progress and allow our end user to update the various ' 
-                + 'arguments to a few pre-defined algorithms.', img: 'mylittletraderbot.jpg'}
-            ]
-        }
+  name: 'projects',
+  components: {},
+  methods: {
+    getImgUrl(pic) {
+      return require('../assets/'+pic)
     }
+  },
+  data() {
+    return {
+      projects: [
+        {
+          title: 'iOS Test Lab App',
+          summary: 'The Tamino iOS Photo application was built to detect deformities and heat ' +
+            'variations of our high temperature furnaces. I built the backend using Swift and AWS tools to connect the user ' +
+            'to the DynamoDB table and upload images to an S3 bucket. From there, a lambda function was triggered and detected ' +
+            'parts using a pre-built neural network.',
+          img: 'takephotoapp.png',
+          github_link: ''
+        },
+        {
+          title: 'My Little Trader Bot',
+          summary: 'My Little Trader Bot began from a desire to learn and understand financial ' +
+            'trading algorithms based on the challenged provided by Optiver at HackDelft 2019 in Delft, Netherlands. ' +
+            'My team researched various types of strategies and played the "fake" stocks, attempting to increase our profits. ' +
+            'We were able to build a front-end website for monitoring our progress and allow our end user to update the various ' +
+            'arguments to a few pre-defined algorithms.',
+          img: 'mylittletraderbot.jpg',
+          github_link: 'https://github.com/osandvold302/hackdelft_s19'
+        },
+        {
+          title: 'Playground News',
+          summary: 'The Univerisity of Missouri hackathon challenged groups to combine journalism and technology to create tools that ' +
+          'would help improve how people receive information. My team imagined a product that would help parents keep track of their ' +
+          'child\'s reading habits, the reading level they are most comfortable with, and topics of interest. We created a web app for ' +
+          'monitoring these statistics and curating relative, age appropriate content for each child.',
+          img: 'playgroundnews.png',
+          github_link: 'https://github.com/osandvold302/KidNews-Tiger18'
+        }
+      ]
+    }
+  }
 }
-
 </script>
 
 <style scoped>
@@ -66,6 +94,37 @@ export default {
 
 .project-rows #project-summary:hover{
     background-color:rgba(115,121,146,.4); 
+}
+
+.img {
+    display: block;
+}
+
+.overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: ease 0.5s;
+    background-color: rgba(115,121,146,.1); 
+}
+
+#project-image:hover .overlay {
+    opacity: 1;
+}
+
+.icon {
+    color: white;
+    position: absolute;
+    top: 35%;
+    bottom: 65%;
+    transform: translate(-50%,-50%);
+    -ms-transform: translate(-50%, -50%);
+    text-align: center;
 }
 
 </style>
