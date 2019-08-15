65b9e3af387823e6a7b0456f50f13da47c375fb3
Olivia Sandvold <ollie302@gmail.com>
2019-08-15 08:44:04 -0500

    Fixed some errors and get path production

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,13 +4,13 @@
         <b-navbar-toggle class="ml-auto navbar-light" target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="navbar-nav ml-auto text-center">
-            <b-nav-item href="/">About</b-nav-item>
-            <b-nav-item href="/">Projects</b-nav-item>
-            <b-nav-item href="/">Resume</b-nav-item>
-          </b-navbar-nav>   
+            <b-nav-item :href="getPathOfEnv()">About</b-nav-item>
+            <b-nav-item :href="getPathOfEnv()">Projects</b-nav-item>
+            <b-nav-item :href="getPathOfEnv()">Resume</b-nav-item>
+          </b-navbar-nav>
         </b-collapse>
         </b-navbar>
-    </div> 
+    </div>
 </template>
 
 <script>
@@ -19,13 +19,20 @@ import { NavbarPlugin } from 'bootstrap-vue'
 Vue.use(NavbarPlugin)
 
 export default {
-    name: 'header-component',
-    components: {}
+  name: 'header-component',
+  components: {},
+  methods: {
+    getPathOfEnv () {
+      return process.env.NODE_ENV //=== 'production'
+        //? '/personal-website'
+        //: '/'
+    }
+  }
 }
 </script>
 
 <style scoped>
-.bg-light{  
+.bg-light{
   background-color: #fffcfe !important;
 }
 
