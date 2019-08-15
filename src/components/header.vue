04b0565f5c8ba2d03b621c70d2930b4676e87735
Olivia Sandvold <ollie302@gmail.com>
2019-08-15 10:09:17 -0500

    Wow I committed a merge conflict my bad

--- src/components/header.vue
+++ src/components/header.vue
@@ -23,13 +23,7 @@ export default {
   components: {},
   methods: {
     getPathOfEnv () {
-<<<<<<< HEAD
-      return process.env.NODE_ENV === 'production'
-        ? '/personal-website'
-        : '/'
-=======
       return process.env.NODE_ENV === 'production' ? '/personal-website' : '/'
->>>>>>> dev
     }
   }
 }
